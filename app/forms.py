00dd0829e14da7266ac8290f021e87ff280364bd
Makarr <maanromanov@edu.hse.ru>
2025-05-28 11:20:45 +0300

    на странице редактирования профиля тоже теперь русский

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,19 @@ from django.contrib.auth.forms import PasswordChangeForm
 from .models import User
 
 class ProfileEditForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label='Имя',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        label='Фамилия',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
